4ead77b503bdb2a9c7988ba1edeef09625bcb1fb
Sashko Milushev <milushev.sashko@gmail.com>
2023-03-18 16:52:38 +0200

    Implement creating a recipe.

--- recipes_notebook/recipes/forms.py
+++ recipes_notebook/recipes/forms.py
@@ -11,3 +11,6 @@ class RecipeForm(forms.ModelForm):
             'description': forms.Textarea(attrs={'rows': 5}),
             'ingredients': forms.Textarea(attrs={'rows': 5}),
         }
+        help_texts = {
+            'ingredients': 'Please separate each ingredient with a comma (e.g. 1 cup sugar, 2 tbsp salt, etc.)',
+        }
