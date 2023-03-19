15be50d7d790b025814b6894d98fc1cfd71c749f
Sashko Milushev <milushev.sashko@gmail.com>
2023-03-19 06:57:37 +0200

    Create dashboard view and template

--- recipes_notebook/common/urls.py
+++ recipes_notebook/common/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from recipes_notebook.common.views import HomeView
+from recipes_notebook.common.views import HomeView, DashboardView
 
 urlpatterns = (
     path('', HomeView.as_view(), name='home'),
+    path('dashboard/', DashboardView.as_view(), name='dashboard'),
 )
--- recipes_notebook/common/views.py
+++ recipes_notebook/common/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.views import generic as views
 
+from recipes_notebook.recipes.models import Recipe
+
 
 class HomeView(views.TemplateView):
     template_name = 'base/home-page.html'
+
+
+class DashboardView(views.ListView):
+    model = Recipe
+    template_name = 'recipes/recipe-card.html'
+    context_object_name = 'recipes'
